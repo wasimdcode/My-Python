34782990aeab7206c5656436135b27e7aaada0db
wasimdcode <pydwasim@gmail.com>
2024-08-22 22:52:30 +0530

    multiply

--- Expressions.py
+++ Expressions.py
@@ -3,8 +3,4 @@ Length = int(input("Enter Length of a Rectangle"))
 Breadth = int(input("Enter Breadth of a Rectangle"))
 Area = Length * Breadth
 print("Area of Reactangle is ", Area)
-#the Area of Triangle formula 1/2bh
-Base = int(input("Enter Base of a Triangle"))
-Height = int(input('Enter Height of a Triangle'))
-Area = 1/2 * Base * Height
-print('Area of Triangle', Area)
+
---
+++ Triangle.py
@@ -0,0 +1,5 @@
+#the Area of Triangle formula 1/2bh
+Base = int(input("Enter Base of a Triangle"))
+Height = int(input('Enter Height of a Triangle'))
+Area = 1/2 * Base * Height
+print('Area of Triangle', Area)
