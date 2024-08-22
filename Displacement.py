1978084727f740eacce51e29c97122d70c16a81a
wasimdcode <pydwasim@gmail.com>
2024-08-23 00:12:59 +0530

    multiply

---
+++ Displacement.py
@@ -0,0 +1,6 @@
+# formula for Displacement
+v = float(input('Enter Initial Velocity'))
+u = float(input('Enter Final Velocity'))
+a = float(input('Enter Accelaration'))
+d = (v**2 - u**2)  / (2 * a)
+print('Displacement Value is ', d)
--- Trapezium.py
+++ Trapezium.py
@@ -1,6 +1,6 @@
 #the Area of Trapezium 1/2(a+b)h
-a = int(input('Enter Value of a'))
-b = int(input('Enter Value of b'))
-h = int(input('Enter Value of h(Height)'))
+a = float(input('Enter Value of a'))
+b = float(input('Enter Value of b'))
+h = float(input('Enter Value of h(Height)'))
 area = 1/2 * (a+b) * h
 print('Area of Trapezium is' , area)
