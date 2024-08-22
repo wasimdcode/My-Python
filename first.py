ab85cc28dc97154f1659ddeae6dacafde76b9b2c
wasimdcode <pydwasim@gmail.com>
2024-08-22 22:55:32 +0530

    multiply

--- first.py
+++ first.py
@@ -5,14 +5,5 @@ print('sum is',c)
 #First Hello World In Python
 print('Hello Guys')
 print('Multiply is',a * b)
-#today i will make simple program using Expressions
-Length = int(input("Enter Length of a Rectangle"))
-Breadth = int(input("Enter Breadth of a Rectangle"))
-Area = Length * Breadth
-print("Area of Reactangle is ", Area)
-#the Area of Triangle formula 1/2bh
-Base = int(input("Enter Base of a Triangle"))
-Height = int(input('Enter Height of a Triangle'))
-Area = 1/2 * Base * Height
-print('Area of Triangle', Area)
+
 
