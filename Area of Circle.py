b84a3ee50c75a210fb190554a45dc9f4e1f25579
wasimdcode <pydwasim@gmail.com>
2024-08-23 16:31:18 +0530

    multiply

---
+++ Area of Circle.py
@@ -0,0 +1,6 @@
+#Area of Circle Pie*r2
+import math
+
+r = float(input('Enter Radius of Circle '))
+Area = math.pi * r * r
+print('Area of Circle is ' , Area )
---
+++ Kilometers into Miles.py
@@ -0,0 +1,4 @@
+#1km = 0.621371 miles
+Km = int (input('Enter Km '))
+Miles = Km * 0.621371
+print('Kilometers In Miles Is ' , Miles )
