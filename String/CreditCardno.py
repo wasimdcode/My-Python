a27d64135ac69dd2252633c1f48549620ee4432f
wasimdcode <pydwasim@gmail.com>
2024-09-08 10:43:57 +0530

    #

---
+++ String/CreditCardno.py
@@ -0,0 +1,14 @@
+# This will Display Credit Card number in "**** **** **** 8374" Format
+card_no = input('Enter 16 Digits Card Number -> ')
+if len(card_no) == 16: #it will check digits 16 
+    last_4 = card_no[12::] #it will give last 4 digits
+    display = '**** ' + '**** ' + '**** ' + last_4 #it will add "* " before last digits
+    print(display)
+else:
+    print('Please ! Enter 16 Digits Number ')
+#Diffirent approach by sir
+'''card_no = input('Enter Card Number ')
+lastdigits = card_no[12::]
+four = '*' * 4 + ' '
+displayno = four * 3 + lastdigits
+print(displayno)'''
---
+++ String/NamefromEmail.py
@@ -0,0 +1,5 @@
+# this will find user id and domain name from email address
+email = input('Entre Email Address -> ')
+f = email.find('@') 
+print('User ID: ', email[:f])
+print('Domain Name:', email[f+1:])
